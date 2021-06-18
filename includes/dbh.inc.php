5a240d328629d08068daad4f8114ce99fa0e6b13
Naddison94 <nathan@skillsforward.co.uk>
2021-06-18 21:02:44 +0100

    testing

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,12 +1,10 @@
 <?php
 
-$serverName ="localhost";
-$dbUserName ="root";
-$dbPassword ="";
-$dbName ="loginsystem";
+function databaseConn() {
+    $serverName ="localhost";
+    $dbUserName ="root";
+    $dbPassword ="";
+    $dbName ="loginsystem";
 
-$conn = mysqli_connect($serverName,$dbUserName,$dbPassword,$dbName);
-
-if(!$conn){
-    die("Connection Error" . mysqli_connect_error());
+    return mysqli_connect($serverName,$dbUserName,$dbPassword,$dbName);
 }
--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -2,49 +2,44 @@
 
 if (isset($_POST["submit"])) {
 
-    $username = $_POST["username"];
-    $emailAdd = $_POST["emailAdd"];
-    $userId = $_POST["userId"];
-    $pwd = $_POST["password"];
-    $rePwd = $_POST["rePassword"];
+$username = $_POST["username"];
+$emailAdd = $_POST["emailAdd"];
+$userId = $_POST["userId"];
+$pwd = $_POST["password"];
+$rePwd = $_POST["rePassword"];
 
-    require_once 'dbh.inc.php';
-    require_once 'functions.inc.php';
+require_once 'dbh.inc.php';
+require_once 'functions.inc.php';
 
-    if (emptyInputSignup($username, $emailAdd, $userId, $pwd, $rePwd) !== false) {
-        header("location: ../signup.php?error=emptyinput");
-        exit();
-    }
-    if (invalidUid($username) !== false) {
-        header("location: ../signup.php?error=invaliduid");
-        exit();
-    }
-    if (invalidEmail($emailAdd) !== false) {
-        header("location: ../signup.php?error=invalidemail");
-        exit();
-    }
-    if (pwdMatch($pwd, $rePwd) !== false) {
-        header("location: ../signup.php?error=pwdmatcherror");
-        exit();
-    }
-    if (uidExists($conn, $username, $emailAdd) !== false) {
-        header("location: ../signup.php?error=usernametaken");
-        exit();
-    }
+if (emptyInputSignup($username, $emailAdd, $userId, $pwd, $rePwd) !== false) {
+    header("location: ../signup.php?error=emptyinput");
+    exit();
+}
 
-    createUser($conn, $username, $emailAdd, $pwd);
-    }
-    else {
-        header("location: ../signup.php");
-        exit();
-    }
+if (invalidUid($username) !== false) {
+    header("location: ../signup.php?error=invaliduid");
+    exit();
+}
 
+if (invalidEmail($emailAdd) !== false) {
+    header("location: ../signup.php?error=invalidemail");
+    exit();
+}
 
+if (pwdMatch($pwd, $rePwd) !== false) {
+    header("location: ../signup.php?error=pwdmatcherror");
+    exit();
+}
 
+$conn = databaseConn();
+if (uidExists($conn, $username, $emailAdd) !== false) {
+    header("location: ../signup.php?error=usernametaken");
+    exit();
+}
 
-    var_dump($_POST[]);
+createUser($conn, $username, $emailAdd, $pwd);
 
-
-    else {
+} else {
     header("location: ../signup.php");
+    exit();
 }
